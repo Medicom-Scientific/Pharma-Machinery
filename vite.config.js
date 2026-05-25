9f404d32cfaa1ff18c9329f6699d36f203a31174
Medicom Group <medicom@medicom-grp.com>
2026-05-25 09:42:22 +0400

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/Pharma-Machinery/',
   plugins: [react()],
   resolve: {
     alias: {
